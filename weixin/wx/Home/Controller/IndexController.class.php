12c13c9623664ff9c0c476ae5330adad6486cf5b
YU <jia6_yu6@163.com>
2016-03-01 16:08:41 +0800

    index

--- weixin/wx/Home/Controller/IndexController.class.php
+++ weixin/wx/Home/Controller/IndexController.class.php
@@ -3,40 +3,57 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
+    public function __construct(){
+        
+    }
+
     public function index(){
-    	$timestamp = $_GET['timestamp'];
-		$nonce = $_GET['nonce'];
-		$token = "weixin";
-		$signature = $_GET['signature'];
-		$arr = array($token,$timestamp,$nonce);
-		sort($arr);
-		$tmpstr = implode('', $arr);
-		$tmpstr = sha1($tmpstr);
-		if($tmpstr == $signature && $_GET['echostr']){
-			echo $_GET['echostr'];
-			exit;
-		}
-		else{
-			$this->responseMsg();
-		}
+        //获得参数 signature nonce token timestamp echostr
+        $nonce     = $_GET['nonce'];
+        $token     = 'imooc';
+        $timestamp = $_GET['timestamp'];
+        $echostr   = $_GET['echostr'];
+        $signature = $_GET['signature'];
+        //形成数组，然后按字典序排序
+        $array = array();
+        $array = array($nonce, $timestamp, $token);
+        sort($array);
+        //拼接成字符串,sha1加密 ，然后与signature进行校验
+        $str = sha1( implode( $array ) );
+        if( $str  == $signature && $echostr ){
+            //第一次接入weixin api接口的时候
+            echo  $echostr;
+            exit;
+        }else{
+            $this->reponseMsg();
+        }
     }
-    public function responseMsg(){
-    	$postArr = $GLOBALS["HTTP_RAW_POST_DATA"];
-    	$postObj = simplexml_load_string($postArr);
-    			$toUser = $postObj->FromUserName;
-    			$fromUser = $postObj->ToUserName;
-    			$time = time();
-    			$MsgType = "test";
-    			$Content = "欢迎关注我们的微信账号.";
-    			$template ="<xml>
- 							<ToUserName><![CDATA[%s]]></ToUserName>
- 							<FromUserName><![CDATA[%s]]></FromUserName> 
- 							<CreateTime>%s</CreateTime>
-							<MsgType><![CDATA[%s]]></MsgType>
- 							<Content><![CDATA[%s]]></Content>
- 							</xml>";
- 				$info = sprintf($template,$toUser,$fromUser,$time,$MsgType,$Content);
- 				echo $info;
-                                                        echo "asd";			
-    }
+    // 接收事件推送并回复
+    public function reponseMsg(){
+        //1.获取到微信推送过来post数据（xml格式）
+        $postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
+        //2.处理消息类型，并设置回复类型和内容
+        $postObj = simplexml_load_string( $postArr );
+        //判断该数据包是否是订阅的事件推送
+        if( strtolower( $postObj->MsgType) == 'event'){
+            //如果是关注 subscribe 事件
+            if( strtolower($postObj->Event == 'subscribe') ){
+                //回复用户消息(纯文本格式) 
+                $toUser   = $postObj->FromUserName;
+                $fromUser = $postObj->ToUserName;
+                $time     = time();
+                $msgType  =  'text';
+                $content  = '欢迎关注我们的微信公众账号'.$postObj->FromUserName.'-'.$postObj->ToUserName;
+                $template = "<xml>
+                            <ToUserName><![CDATA[%s]]></ToUserName>
+                            <FromUserName><![CDATA[%s]]></FromUserName>
+                            <CreateTime>%s</CreateTime>
+                            <MsgType><![CDATA[%s]]></MsgType>
+                            <Content><![CDATA[%s]]></Content>
+                            </xml>";
+                $info     = sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
+                echo $info;            
+
+            }
+        }
 }
